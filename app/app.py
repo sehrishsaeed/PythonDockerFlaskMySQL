9557cb406a9715509422573bfd1d04ea5d446835
“your_github_username” <78240872+sehrishsaeed@users.noreply.github.com>
2021-05-12 22:22:25 -0400

    feature: added code for data to app.py

--- app/app.py
+++ app/app.py
@@ -5,7 +5,7 @@ from flask import render_template
 from flaskext.mysql import MySQL
 from pymysql.cursors import DictCursor
 
-app = Flask(__name__)
+app =Flask(__name__)
 mysql = MySQL(cursorclass=DictCursor)
 
 app.config['MYSQL_DATABASE_HOST'] = 'db'
